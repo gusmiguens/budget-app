13fb0bf7c604bf687ced89f62ba2836f89803548
Gustavo Miguens <gustavo.miguens@fluxit.com.ar>
2020-06-06 19:42:02 -0300

    Final version app with styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,11 @@ import { startLogin } from '../actions/auth'
 
 export const Login = ({ startLogin }) => (
     <div className="box-layout">
-        <button onClick={startLogin}>Login</button>
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Budget App</h1>
+            <p>It's time to take control of your budget</p>
+            <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
     </div>
 )
 
